e95e9a1476a9f7788fd0b043cf9bcbc20ff921fd
Ershad <ershad.moh37@gmail.com>
2026-02-03 14:06:40 +0100

    moved to its folder

---
+++ greedy-algorithms/min-groups/min_groups.py
@@ -0,0 +1,29 @@
+def main():
+    children = [2,2,3,4,4,5,6,6,6,7,8,9]
+    groups = grouping_the_children(min_num_of_groups(children), children)
+    print(groups)
+    
+
+
+def min_num_of_groups(ages: list[int]):
+    groups = []
+    i = 0
+    while i < len(ages):
+        l, r = ages[i], ages[i] + 2
+        groups.append((l, r))
+        while i < len(ages) and r >= ages[i]:
+            i += 1
+
+    return groups
+
+
+def grouping_the_children(groups: list[int], arr: list[int]):
+    for group in groups:
+        return arr[group[0]: group[1]+ 1]
+    
+
+
+
+
+if __name__ == "__main__":
+    main()
